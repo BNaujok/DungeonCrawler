4df2de412fe08cdb0284d2bcc56acc435dc978e2
BNaujok <bindynaujok@hotmail.com>
2017-03-19 14:46:51 +1100

    Cleaned up the object declarations
    - Names following PEP8 protocol
    - Removed original declarations as could not be changed
    - Removed extra def functions, as they were not being used.

--- character.py
+++ character.py
@@ -1,29 +1,16 @@
 # character object, will initially set up the variables such as race and name for the user, but the important
 # part is to make sure that the list is functional and can have objects added to it as items are secured in the dungeon.
 
-class character(object):
+# 19/2/17 Cleaned up all of the objects created here.
 
-    name = "Brain"
-    race = "Oxymoron warrior"
-    bag = [0]*10
-
+class Character(object):
     def __init__(self, name, race, bag):
         self.name = name
-        self.age = race
-        self.major = bag
+        self.race = race
+        self.bag = bag
 
-def make_character(name, race, bag):
-    character = character(name, age, major)
-    return character
-
-
-class monster(object):
-    name = ""
-
-    def _init_(self, name):
+class Monster(object):
+   def _init_(self, name, HP):
         self.name = name
-
-def make_dungeonrat(name):
-    monster = monster("dungeon rat")
-    return monster
+        self.HP = HP
 
